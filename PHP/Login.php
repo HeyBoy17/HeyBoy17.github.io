4bf48dc7308160714e1edc6f9e706062d2eb349d
HeyBoy <69630932+HeyBoy17@users.noreply.github.com>
2025-03-26 11:33:00 +0100

    Initial Upload

---
+++ PHP/Login.php
@@ -0,0 +1,29 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Read the credentials from the text file
+    $credentials = file('C:\xampp\htdocs\Buitenhuis\HiLo_Game\Documents\Login_Credentials.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $isValid = false;
+
+    foreach ($credentials as $credential) {
+        list($fileUser , $filePass) = explode(':', $credential);
+        if ($fileUser  === $username && $filePass === $password) {
+            $isValid = true;
+            break;
+        }
+    }
+
+    if ($isValid) {
+        header("Location: /Buitenhuis/HiLo_Game/Home.html");
+        exit();
+    } else {
+        echo "Invalid username or password.";
+    }
+} else {
+    echo "Please submit the form.";
+}
+
+?>
